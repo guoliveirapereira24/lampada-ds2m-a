c1539973c46f9e61e742cb07e58ae0b679c02a06
GUSTAVO DE OLIVEIRA PEREIRA <21193137@CFP127ALUNO.LOCAL>
2021-08-25 09:48:33 -0300

    Botões liga e desliga concluidos

--- lampada.js
+++ lampada.js
@@ -2,11 +2,21 @@
 
 function lampOn() {
     const lampada = document.getElementById("lampada")
+    const botaoLigar = document.getElementById("ligar")
+    const botaoDesligar = document.getElementById("desligar")
+
     lampada.src = "img/ligada.jpg"
+    botaoLigar.disabled = true
+    botaoDesligar.disabled = false
 }
 function lampOff(){
     const lampada = document.getElementById("lampada")
+    const botaoLigar = document.getElementById("ligar")
+    const botaoDesligar = document.getElementById("desligar")
+
     lampada.src = "img/desligada.jpg"
+    botaoLigar.disabled = false
+    botaoDesligar.disabled = true
 }
 // eventos
 document.getElementById('ligar')
