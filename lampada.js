72400d3815e3bee198643a0b69dc5773addba17b
GUSTAVO DE OLIVEIRA PEREIRA <21193137@CFP127ALUNO.LOCAL>
2021-08-25 10:39:44 -0300

    refatorando

--- lampada.js
+++ lampada.js
@@ -1,25 +1,57 @@
 "use strict"
+const lampada = document.getElementById("lampada")
+
+
+function botoesLigaDesliga(estadoLiga, estadoDesliga) {
+    const botaoLigar = document.getElementById("ligar")
+    const botaoDesligar = document.getElementById("desligar")
+    botaoLigar.disabled = estadoLiga
+    botaoDesligar.disabled = estadoDesliga
+
+}
+
+function lampBreak(){
+    return lampada.src.includes("quebrada")
+}
+
 
 function lampOn() {
     const lampada = document.getElementById("lampada")
-    const botaoLigar = document.getElementById("ligar")
-    const botaoDesligar = document.getElementById("desligar")
+  //  const botaoLigar = document.getElementById("ligar")
+    //const botaoDesligar = document.getElementById("desligar")
 
-    lampada.src = "img/ligada.jpg"
-    botaoLigar.disabled = true
-    botaoDesligar.disabled = false
+    if(!lampBreak()){
+        lampada.src = "img/ligada.jpg"
+        botoesLigaDesliga(true, false)
+    }
 }
 function lampOff(){
     const lampada = document.getElementById("lampada")
-    const botaoLigar = document.getElementById("ligar")
-    const botaoDesligar = document.getElementById("desligar")
+  //  const botaoLigar = document.getElementById("ligar")
+    //const botaoDesligar = document.getElementById("desligar")
 
-    lampada.src = "img/desligada.jpg"
-    botaoLigar.disabled = false
-    botaoDesligar.disabled = true
+    if(!lampBreak()){
+        lampada.src = "img/desligada.jpg"
+        botoesLigaDesliga(false, true)
+    }
 }
+function lampBreak(){
+  //  const botaoLigar = document.getElementById("ligar")
+    //const botaoDesligar = document.getElementById("desligar")
+
+    lampada.src = "img/quebrada.jpg"
+    botoesLigaDesliga(true, true)
+}
+
+
 // eventos
 document.getElementById('ligar')
     .addEventListener("click", lampOn)
 document.getElementById('desligar')
     .addEventListener("click", lampOff)    
+document.getElementById("lampada")
+    .addEventListener("mouseover", lampOn)
+document.getElementById("lampada")
+    .addEventListener("mouseleave", lampOff)
+document.getElementById('lampada')
+    .addEventListener("dblclick", lampBreak)
